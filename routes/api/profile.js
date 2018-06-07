a5294f5dcafeff0a205af82bd7a5045b03042cf8
Md Fazlul Karim <fazlulkarimrocky@gmail.com>
2018-06-06 23:08:44 -0700

    Changes profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // routes   GET api/profiles/test
 // desc     Tests post route
-// access   Public
+// access   Public/Private
 
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
